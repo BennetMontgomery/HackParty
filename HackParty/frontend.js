891116d9b703a4e96a262915ffaaafa05c01109e
Christopher Goel <christopher.goel@gmail.com>
2018-01-28 01:52:56 -0500

    NIBBAS

---
+++ HackParty/frontend.js
@@ -0,0 +1,53 @@
+// import * as backend from "backend.js";
+var readyToSend = false;
+$('#password, #confirmPassword').on('keyup', function () {
+	if ($('#password').val().length > 7) {
+		if ($('#password').val() == $('#confirmPassword').val()) {
+			$('#message').html('Passwords match').css('color', 'green');
+			passwordOK = true;
+		}
+		else {
+			$('#message').html('Password is not matching').css('color', 'red');
+			passwordOK = false;
+    }
+	}
+	else {
+		$('#message').html('Password must be more than 7 characters').css('color', 'red');
+		passwordOK = false;
+	}
+	
+});
+
+$('#first_name, #last_name, #username, #email, #password, #confirmPassword').on('keyup', function() {
+	if ($('#first_name').val().length == 0 
+		|| $('#last_name').val().length == 0
+		|| $('#username').val().length == 0
+		|| $('#email').val().length == 0
+		|| $('#password').val().length == 0
+		|| $('#confirmPassword').val().length == 0 
+		|| !passwordOK) {
+		document.getElementById("submit").classList.add('disabled');
+		readyToSend = false;
+	}
+	else {
+		document.getElementById("submit").classList.remove('disabled');
+		readyToSend = true;
+	}
+});
+
+// THIS FUNCTION SENDS INFORMATION FROM FRONTEND TO BACKEND, WORKS ONLY WHEN SUBMIT BUTTON IS CLICKED
+function send() {
+	if (readyToSend) {
+		firstName = $('#first_name').val();
+		lastName = $('#last_name').val();
+		username = $('#username').val();
+		email = $('#email').val();
+		password = $('#password').val();
+	}
+}
+
+
+
+
+
+
--- backend.js
+++ backend.js
@@ -1,17 +1,52 @@
 //DECLARATIONS
 // You may refer to any of these strings because they are set by the frontend.js
-var userRefs = firebase.database().ref("users/");
+var userRefs = firebase.database().ref('users/');
 var firstName = "";
 var lastName = "";
 var username = "";
 var email = "";
 var password = "";
+var valid = false;
+
 firebase.auth().signInWithEmailAndPassword("bennetlogan@gmail.com", "creation").catch(function(error) {
 	console.log(error.message);
 });
-userRefs.once("value", function(snapshot) {
-	console.log(snapshot);
-});
+// userRefs.once("value", function(snapshot) {
+// 	console.log(snapshot);
+// });
+
+
+
+function gotData(data) {
+	var rawData = data.val();
+	var usernames = Object.keys(rawData);
+	for (var i = 0; i < usernames.length; i++) {
+		var k = usernames[i];
+		var pass = rawData[k].password;
+		var firstName = rawData[k].firstName;
+		valid = false;
+		if (k == username && pass == password) {
+			valid = true;
+		}
+		console.log("Password: " + pass + ", First Name: " + firstName, ", Found: " + valid)
+		valid = valid;
+	}
+
+	return valid
+}
+
+function errData(err) {
+	console.log("Error!")
+}
+
+function check(username, password) {
+	this.username = username;
+	this.password = password;
+	return userRefs.once('value', gotData).then(function (result) {return result});
+}
+
+console.log(check("benito", "creation"))
+
 function submitForm(e) {
 	e.preventDefault();
 	firstName = getInputVal('first_name');
@@ -31,3 +66,5 @@ function submitForm(e) {
 function getInputVal(id) {
 	return document.getElementById(id).value;
 }
+
+console.log(valid);
